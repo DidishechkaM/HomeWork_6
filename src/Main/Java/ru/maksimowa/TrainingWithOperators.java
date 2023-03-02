a07505889b580f6733a9d13449498aae193e4fe1
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-03-02 13:00:39 +0300

    added new part

--- src/Main/Java/ru/maksimowa/TrainingWithOperators.java
+++ src/Main/Java/ru/maksimowa/TrainingWithOperators.java
@@ -4,6 +4,7 @@ package ru.maksimowa;
 public class TrainingWithOperators {
 
     public static void main(String[] args){
+
     // Присвоение
     int a = 10;
     int b = 3;
@@ -26,26 +27,22 @@ public class TrainingWithOperators {
     System.out.println("a!=b="+(a!=b));
 
     //Переполнение
-        int c =126;
-        int d = (byte)(c+3);
-        System.out.println(d);
-    //Логические операторы
-        boolean f =true;
-        boolean g =false;
+     int c =126;
+     int d = (byte)(c+3);
+     System.out.println(d);
 
+     int h = 130;
+     double i = 10.99;
+     System.out.println("h + i = " + (h+i));
+     System.out.println("h - i = " + (h-i));
 
-
-
-
-
+     //Логические операторы
+      boolean f =true;
+      boolean g =false;
+      System.out.println("f && g="+(f && g));
+      System.out.println("f || t = " + (f || g));
+      System.out.println("!g = " + (!g));
     }
-
-
-
-
-
-
-
 }
 
 
